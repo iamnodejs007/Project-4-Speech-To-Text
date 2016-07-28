0cf442dd17dca1cc501b9162fd27d71c98e19104
lawrencegomez <lfg2305@gmail.com>
2016-07-28 16:02:10 -0700

    recreate User.js

---
+++ models/User.js
@@ -0,0 +1,30 @@
+var
+  mongoose = require('mongoose'),
+  findOrCreate = require('mongoose-findorcreate'),
+  bcrypt = require('bcrypt-nodejs'),
+  Schema = mongoose.Schema,
+  userSchema = new Schema({
+    local: {
+      name: String,
+      email: String,
+      password: String
+    }},
+    {
+      timestamps: true
+  })
+
+  userSchema.plugin(findOrCreate)
+
+  userSchema.methods.generateHash = function(password){
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8))
+    //hashSync prevent other shits running before this happens
+  }
+
+  userSchema.methods.validPassword = function(password){
+    return bcrypt.compareSync(password, this.local.password)
+    //compareSync
+  }
+
+var User = mongoose.model('User', userSchema)
+
+module.exports = User
